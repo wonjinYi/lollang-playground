ebdba3031e0274b0ac642f86a0e74cc135a35f8e
wonjinYi <studioplug17@gmail.com>
2022-03-12 17:29:29 +0900

    front template is done

--- front/README.md
+++ front/README.md
@@ -2,3 +2,8 @@
 
 사용자 입력으로 .lo코드를 받고, 이를 백엔드 코드를 통해 실행합니다.
 실행된 결과는 프론트엔드 상에서 사용자에게 표시됩니다.
+
+빌려온 것
+* Svelte with node.js & rollup
+* CodeMirror
+* Axios
--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,30 +1,115 @@
 <script>
-	export let name;
+	
+
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div id="app">
+	<header>
+		<h1 class="header-title">Lollang Playground</h1>
+		<div class="header-functions" style="margin-left:8px;">
+			<button class="header-functions-btn">DOWNLOAD</button>
+			<button class="header-functions-btn">RUN</button>
+		</div>
+		<div class="header-lollang-github" style="margin-left:auto;">
+			<button class="header-functions-btn" 
+				on:click={e=>{window.open('https://github.com/riroan/lollang')}}>
+					lollang GITHUB
+			</button>
+		</div>
+	</header>
+
+	<main>
+		<section class="main-input-part">
+			<h2 class="main-subtitle">CODE</h2>
+			<div class="main-article">
+				<textarea class="code-editor" placeholder="code your lol here" />
+			</div>
+		</section>
+		<div style="border-right: 2px solid lightgray;"></div>
+		<section class="main-output-part">
+			<h2 class="main-subtitle">RESULT</h2>
+			<div class="main-article">
+				<p class="result-viewer">Output of lollang code will be shown here</p>
+			</div>
+			
+		</section>
+	</main>
+</div>
 
 <style>
+	#app {
+		display : flex; 
+		flex-direction: column;
+		height : 100%;
+		width : 100%;
+	}
+
+	header {
+		display : flex;
+		align-items: center;
+		
+		height : 48px;
+		padding : 0 8px;
+		background-color : lightgray;
+	}
+
+	.header-title {
+		margin : 0;
+		font-size : 24px;
+		font-weight: lighter;
+	}
+
+	.header-functions-btn {
+		margin : 0;
+	}
+
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		display : flex;
+		flex : 1;
+
+		width : 100%;
+		max-width : 100%;
+		border-left : 4px solid lightgray;
+		border-right : 4px solid lightgray;
+		border-bottom : 4px solid lightgray;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	main > section {
+		display : flex;
+		flex-direction: column;
+		flex : 1;
+		height : 100%;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.main-subtitle {
+		padding-top : 8px;
+		font-size : 18px;
+		margin : 0;
+		font-weight : lighter;
+		text-align : center;
+	}
+
+	.main-article {
+		flex : 1;
+		padding : 8px;
+	}
+
+	.code-editor {
+		resize: none;
+		border : none;
+		width : 100%;
+		height : 100%;
+		background-color : #ffe0e0;
+		margin : 0;
+	}
+
+	.code-editor:focus {
+    	outline: none;
+	}
+
+	.result-viewer {
+		margin : 0;
+		word-wrap: break-word;
 	}
 </style>
